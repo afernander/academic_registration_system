262ccfe95ab124c63e80bef0da0d431645469567
Alejandro Fernandez <alejandro.fernandezr@perficient.com>
2022-08-02 16:03:03 -0500

    Services Interfaces created

--- src/main/java/com/perficient/path/practice/academic_registration_system/services/CourseService.java
+++ src/main/java/com/perficient/path/practice/academic_registration_system/services/CourseService.java
@@ -1,5 +1,21 @@
 package com.perficient.path.practice.academic_registration_system.services;
 
+import java.util.Set;
+
+import com.perficient.path.practice.academic_registration_system.models.Course;
+import com.perficient.path.practice.academic_registration_system.models.Subject;
+import com.perficient.path.practice.academic_registration_system.models.User;
+
 public interface CourseService {
+    
+    Course getCourseById(Long id);
+    Set<Course> getAllCourses();
+    Course createCourse(Course course);
+    Course updateCourse(Course course);
+    void deleteCourseById(Long id);
+    Set<Course> getCoursesByName(String name);
+    Set<Subject> getSubjectsByCourseId(Long courseId);
+    Set<User> getUsersByCourseId(Long courseId);
+    String getCourseDurationByCourseId(Long courseId);
     
 }
--- src/main/java/com/perficient/path/practice/academic_registration_system/services/UserService.java
+++ src/main/java/com/perficient/path/practice/academic_registration_system/services/UserService.java
@@ -1,5 +1,17 @@
 package com.perficient.path.practice.academic_registration_system.services;
 
+import java.util.Set;
+
+import com.perficient.path.practice.academic_registration_system.models.Course;
+import com.perficient.path.practice.academic_registration_system.models.User;
+
 public interface UserService {
-    
+
+    User getUserById(Long id);
+    Set<User> getAllUsers();
+    User createUser(User user);
+    User updateUser(User user);
+    void deleteUserById(Long id);
+    Set<User> getUsersByFirstName(String fistName);
+    Set<Course> getCoursesByUserId(Long userId);
 }
